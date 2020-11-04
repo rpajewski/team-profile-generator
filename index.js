0a2ce30f948e86ae268b5e50953bc2196df62448
Ricky Bobby <richard.pajewski@gmail.com>
2020-11-04 15:02:57 -0600

    added utils for generating html and copying over css

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const generatePage = require('./src/page-template');
+
+const promptUser = () => {
+    
+}
+
+// promptUser()
+//   .then(employeeData => {
+//     return generatePage(employeeData);
+//   })
+//   .then(pageHTML => {
+//     return writeFile(pageHTML);
+//   })
+//   .then(writeFileResponse => {
+//     console.log(writeFileResponse);
+//     return copyFile();
+//   })
+//   .then(copyFileResponse => {
+//     console.log(copyFileResponse);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
